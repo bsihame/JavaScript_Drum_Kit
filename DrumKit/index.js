dc18b70b51b19fff09bea5dc88941aeb7aa67537
bsihame <sihamebazi@pursuit.org>
2021-01-13 16:35:54 -0500

    add src to the sounds

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
- console.log("hello")
+  const removeTransition = (e) => {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing')
+  }
+  const playSound=(e) =>{
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    console.log(audio)
+    if (!audio) return;
+
+    // const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+		// const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		// if (!audio) return;
+    key.classList.add('playing')
+    audio.currentTime = 0;
+    audio.play();
+  }
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key=> key.addEventListener('transitioned', removeTransition))
+  window.addEventListener('keydown', playSound);
 });
